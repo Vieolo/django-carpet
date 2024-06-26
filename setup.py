83c0ab0638946a1fa08d2492c4d8f14355508df5
Ramtin <Ramtin.abadi@gmail.com>
2024-06-26 15:03:40 +0300

    Version Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django_carpet",  # Replace with your own username
-    version="0.2.0",
+    version="0.2.1",
     author="Vieolo",
     description="Foundational functionalities for Django",
     long_description=long_description,
